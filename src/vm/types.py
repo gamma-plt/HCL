b1ee024f3f766a9c71fa265cfcfb315026434daa
scvalencia <sc.valencia606@uniandes.edu.co>
2016-01-18 18:43:49 -0500

    The native types supported by the machine

---
+++ src/vm/types.py
@@ -0,0 +1,40 @@
+class Integer(object):
+
+	def __init__(self, binrep):
+		self.binrep = binrep
+
+	def convert(self):
+		return None
+
+class Real(object):
+
+	def __init__(self, binrep):
+		self.binrep = binrep
+
+	def convert(self):
+		return None
+
+class Boolean(object):
+
+	def __init__(self, binrep):
+		self.binrep = binrep
+
+	def convert(self):
+		return None
+
+class Char(object):
+
+	def __init__(self, binrep):
+		self.binrep = binrep
+
+	def convert(self):
+		return None
+
+class List(object):
+
+	def __init__(self, binrep):
+		self.binrep = binrep
+
+	def convert(self):
+		return None
+
