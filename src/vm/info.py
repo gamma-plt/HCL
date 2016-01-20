437fb9dd1791c470008c163897d627ef35430e71
scvalencia <sc.valencia606@uniandes.edu.co>
2016-01-20 16:03:08 -0500

    Outupu and error handling features added

--- src/vm/info.py
+++ src/vm/info.py
@@ -1,5 +1,8 @@
+'''Set to True to turn on debugging mode.'''
+DEBUGGING = True
+
 def print_usage():
-	print 'Usage: vhcl <option> file_name'
+	print 'USAGE: vhcl <option> file_name'
 
 def print_file_not_found(filename):
 	print 'FILE NOT FOUND: ' + filename
