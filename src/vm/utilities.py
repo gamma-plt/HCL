b01b466c2fd353cc305bdd7a8ee345645283b928
scvalencia <sc.valencia606@uniandes.edu.co>
2016-01-20 16:04:19 -0500

    Utilities for type and hw handling

---
+++ src/vm/utilities.py
@@ -0,0 +1,16 @@
+class TypeUtility(object):
+
+	def __init__(self):
+		pass
+
+	def int2vmbin(self, integer):
+		pass
+
+	def real2vmbin(self, real):
+		pass
+
+	def boolean2vmbin(self, boolean):
+		pass
+
+	def char2vmbin(self, char):
+		pass
