bd0a87c5505bbbb4c433d5bc66f467f1da8a57b6
scvalencia <sc.valencia606@uniandes.edu.co>
2016-01-27 17:06:14 -0500

    Global variable organization and module enhancement

---
+++ src/vm/globalenv.py
@@ -0,0 +1,12 @@
+# The number of words in the memory
+
+MEMORY_SIZE = 34
+
+# The number of bits of a word, not counting the occupation bit
+
+'''Defines the number of bits for each word.
+This must not take into account the occupation bit.
+That its a flag to indicates wheter or not the word
+is occuppied'''
+
+WORD_SIZE = 16
