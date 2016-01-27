9feefd4dbafe805e2c3f457e670e135637209a31
scvalencia <sc.valencia606@uniandes.edu.co>
2016-01-27 17:12:36 -0500

    New utilities for data conversion and representation

--- src/vm/utilities.py
+++ src/vm/utilities.py
@@ -14,3 +14,23 @@ class TypeUtility(object):
 
 	def char2vmbin(self, char):
 		pass
+
+def isbinarystring(string):
+	for ch in string:
+		if ch not in ['0', '1']:
+			return False
+	return True
+
+def compound_sizeof(type_name):
+	value = 0
+	chunks = 0
+
+	if '#' in type_name:
+		idx = type_name.index('#')
+		value = type_name[idx + 1:]
+		chunks = 1
+
+		if '#' in value:
+			value, chunks = value.split('#')
+
+	return int(value) * int(chunks)
