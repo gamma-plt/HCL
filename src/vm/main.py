acf2de41af11cad61b022f55f1e83e70f7becf52
scvalencia <sc.valencia606@uniandes.edu.co>
2016-03-05 23:34:23 -0500

    Main driver stuff for processing anr running a VM code file

--- src/vm/main.py
+++ src/vm/main.py
@@ -1,39 +1,14 @@
-# RUN AS: python main.py ../../examples/vm/bubble_sort.vhcl
+# RUN AS: python main.py ../../examples/vm/example1.vhcl
 
 import os
 import sys
 import info
 import lang
+import hardware
 import debugger
+import globalenv
 
-MEMORY_SIZE = 34
-WORD_SIZE = 16
-
-mem = ['0' * WORD_SIZE for i in range(MEMORY_SIZE)]
-amv = {'summation' : ('BOOL', '0x20')}
-args = []
-world = ['add', 'ret', 'r1', 'r2', 'r3', 'r4', 'r5']
-
-def execute(instruction):
-	index, instruction = instruction
-	inst_args = instruction.split(' ')
-
-	print inst_args
-
-	if len(inst_args) == 1:
-		pass	
-	elif len(inst_args) == 2:
-		pass
-	elif len(inst_args) == 3:
-		pass
-	else:
-		pass
-
-
-def execute_instructions(instructions):
-	for instruction in instructions:
-		execute(instruction)
-		print
+MEMORY_SIZE = globalenv.MEMORY_SIZE
 
 def remove_comments(parsed_file):
 	ans = []
@@ -53,7 +28,7 @@ def remove_comments(parsed_file):
 def get_blocks(fileobject):
 	lines = [(i, line) for i, line in enumerate(fileobject) if line != '\n']
 	lines = remove_comments(lines)	
-	blocks = [(i, line) for i, line in lines if line[0] != '\t']
+	blocks = [(i, line) for i, line in lines if line and line[0] != '\t']
 
 	lower_bounds = [index for index, line in blocks]
 	upper_bounds = lower_bounds[1:] + [lower_bounds[-1]]
@@ -74,13 +49,25 @@ def get_blocks(fileobject):
 def process_file(filename):
 	fileobject = open(filename, 'r')
 	blocks = get_blocks(fileobject)
-	_, halt_ist = blocks[-1][0]
 
+	_, halt_ist = blocks[-1][0]
 	if halt_ist != lang.HALT_INSTRUCTION:
 		lang.report_syntax_error(_)
 
-def main():
+	whole_program = []
 
+	for block in blocks:
+		for blck in block:
+			whole_program.append(blck)
+
+	syntax_tree = lang.get_syntax_tree(whole_program)
+	
+	for node in syntax_tree.children:
+		lang.check_syntax_tree(node)
+
+	return syntax_tree.children
+
+def main():
 	arguments = sys.argv
 	if len(arguments) != 2:
 		info.print_usage()
@@ -89,13 +76,12 @@ def main():
 		if not os.path.isfile(filename):
 			info.print_file_not_found(filename)
 		else:
-			process_file(filename)
-			
-	'''
-	dbg = debugger.Debugger(mem, amv, args, world, MEMORY_SIZE)
-	dbg.execute('pamv')
-	'''
+			vm = hardware.HCLVirtualMachine()
+			syntax_tree = process_file(filename)
+			vm.process_syntax_tree(syntax_tree)
 
+			db = debugger.Debugger(vm, MEMORY_SIZE)
+			db.run()
 
 if __name__ == '__main__':
 	main()
