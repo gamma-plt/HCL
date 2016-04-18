e20da0acef3dba5222762b80f4d6d11c83512bce
scvalencia <sc.valencia606@uniandes.edu.co>
2016-04-18 07:37:09 -0500

    Debugging machine level code

--- src/vm/globalenv.py
+++ src/vm/globalenv.py
@@ -1,6 +1,6 @@
 # The number of words in the memory
 
-MEMORY_SIZE = 10000
+MEMORY_SIZE = 40
 
 # The number of bits of a word, not counting the occupation bit
 
@@ -9,4 +9,4 @@ This must not take into account the occupation bit.
 That its a flag to indicates wheter or not the word
 is occuppied'''
 
-WORD_SIZE = 32
+WORD_SIZE = 128
