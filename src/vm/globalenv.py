adde775189ad64d114cb6535c188b9decc645561
scvalencia <sc.valencia606@uniandes.edu.co>
2016-04-19 22:42:48 -0500

    Increasing memory size

--- src/vm/globalenv.py
+++ src/vm/globalenv.py
@@ -1,6 +1,6 @@
 # The number of words in the memory
 
-MEMORY_SIZE = 1000
+MEMORY_SIZE = 2 ** 10
 
 # The number of bits of a word, not counting the occupation bit
 
@@ -9,4 +9,4 @@ This must not take into account the occupation bit.
 That its a flag to indicates wheter or not the word
 is occuppied'''
 
-WORD_SIZE = 128
+WORD_SIZE = 16
