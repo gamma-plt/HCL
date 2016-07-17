6b1ecf7a1ad21a93c961592eba6c10d7f6312e2f
andfoy <andfoy@gmail.com>
2016-07-17 18:49:20 -0500

    Merging branches

--- src/compile.py
+++ src/compile.py
@@ -1,7 +1,7 @@
 from compiler.backend import codegen
 from compiler.frontend import semantic
 
-path = '../examples/hcl/fibonacci.hcl'
+path = '../examples/hcl/collatz.hcl'
 
 stat, tree, data = semantic.analyse(path)
 stat, lines = codegen.code_generation(data)
