07eb44a18e476aae44f124c7014206eb18d9372a
scvalencia <sc.valencia606@uniandes.edu.co>
2016-07-18 18:54:49 -0500

    Updating the capabilities of the VM

--- src/compile_aux.py
+++ src/compile_aux.py
@@ -1,7 +1,9 @@
+import sys
+
 from compiler.backend import codegen
 from compiler.frontend import semantic
 
-path = '../examples/hcl/final/maximum.hcl'
+path = '../examples/hcl/final/'
 
-stat, tree, data = semantic.analyse(path)
+stat, tree, data = semantic.analyse(path + sys.argv[1])
 stat, lines = codegen.code_generation(data)
